12f7d6258283b5483aa5c67e10801b11f3348273
MichalLinek <michal.linek89@gmail.com>
2023-08-15 20:47:59 +0200

    Transition to typescript

--- create-options.js
+++
@@ -1,9 +0,0 @@
-createOptions = (fileDatabase) => {
-  
-  return Object.keys(fileDatabase).map(file => ({
-      name: fileDatabase[file].name,
-      value: file
-    }))
-  }
-
-module.exports = createOptions;
